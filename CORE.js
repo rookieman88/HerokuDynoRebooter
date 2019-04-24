a65ebe889113ce5423111c09fa86519632cb54d5
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-04-24 20:31:11 +0900

    Create CORE.js

---
+++ CORE.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const app = new Discord.Client();
+const Heroku = require('heroku-client')
+const heroku = new Heroku({ token: process.env.THETOKEN })
+app.login(process.env.BotToken)
+
+app.on('message', (message) => {
+
+	if(message.content.startsWith(`워터봇 리붓 자스`)){
+    if (message.author.id === '님ID_Here') {
+      heroku.delete('/apps/앱이름/dynos/worker')
+ 
+      message.channel.send('처리완료')
+    } else {
+      message.channel.send('관리자로 등록되어있지 않습니다.')
+    }
+}
+
+});
